484044e681af8183eafabecfbdaf10fdb752b158
shaowenxing <shaowenxing@dyx.cn>
2021-05-18 16:30:25 +0800

    2021-05-18.16:30:25自动打包

--- test.sh
+++ test.sh
@@ -1,21 +1,15 @@
-#!/bin/bash
+#!/bin/bash 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TIME="date +%Y-%m-%d.%H:%M:%S"
 # ////////////////////////////////////// aliyun
 cd ../micrqwe
-pwd
 rm -rf *
-pwd
 cd ../micrqwe.github.io
-pwd
+hexo clean
 hexo deploy
-echo "zhixing deploy"
 cd public
-echo "sfdsf"
 cp -r  * ../../micrqwe/
-echo "shanchuwenjian"
 cd ../../micrqwe
-pwd
 git add .
 git commit -m "`$TIME`自动打包"
 git push
