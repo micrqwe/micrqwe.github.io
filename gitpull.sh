9d1cde7c6a527f52415ffec76b57710f50badae8
shaowenxing <shaowenxing@dyx.cn>
2021-05-21 20:29:28 +0800

    2021-05-21.20:29:28自动打包

--- gitpull.sh
+++ gitpull.sh
@@ -2,11 +2,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TIME="date +%Y-%m-%d.%H:%M:%S"
 # ////////////////////////////////////// aliyun
+hexo clean
+hexo deploy
 cd ../micrqwe
 rm -rf *
 cd ../micrqwe.github.io
-hexo clean
-hexo deploy
 cd public
 cp -r  * ../../micrqwe/
 cd ../../micrqwe
@@ -17,9 +17,10 @@ cd ../micrqwe.github.io
 git add .
 git commit -m "`$TIME`自动打包"
 git push
-git checkout master
+cd ../micrqwe-master
+rm -rf *
 cp -rf ../micrqwe/* .
 git add .
 git commit -m "`$TIME`自动打包"
 git push -f
-git checkout hexo-master-build
+cd ../micrqwe.github.io
--- gitpullbak.sh
+++ gitpullbak.sh
@@ -2,14 +2,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TIME="date +%Y-%m-%d.%H:%M:%S"
 # ////////////////////////////////////// aliyun
-cd ../micrqwe
-rm -rf *
-cd ../micrqwe.github.io
 hexo clean
 hexo deploy
-cd public
-cp -r  * ../../micrqwe/
-cd ../../micrqwe
+cd ../micrqwe
+rm -rf *
+cp -rf ../micrqwe.github.io/public/* .
 git add .
 git commit -m "`$TIME`自动打包"
 git push
@@ -17,9 +14,10 @@ cd ../micrqwe.github.io
 git add .
 git commit -m "`$TIME`自动打包"
 git push
-git checkout master
+cd ../micrqwe-master
+rm -rf *
 cp -rf ../micrqwe/* .
 git add .
 git commit -m "`$TIME`自动打包"
 git push -f
-git checkout hexo-master-build
+cd ../micrqwe.github.io
